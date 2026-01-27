c726294cc22dea05c7688336d85e8d5087c97376
Touri Aida <touri1705@outlook.com>
2026-01-27 09:20:46 +0900

    OHTMLのホットリロード対応

--- src/nexom/assets/app/pages/_templates.py
+++ src/nexom/assets/app/pages/_templates.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 from nexom.app.template import ObjectHTMLTemplates
 
-from __app_name__.config import TEMPLATES_DIR
+from __app_name__.config import TEMPLATES_DIR, RELOAD
 
-templates = ObjectHTMLTemplates(base_dir=TEMPLATES_DIR)
+templates = ObjectHTMLTemplates(base_dir=TEMPLATES_DIR, reload=RELOAD)
